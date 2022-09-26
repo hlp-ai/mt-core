962ec5fb956fd78ccef634b30daf238a342d1a90
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-09-26 10:34:55 +0800

    Add tag translation example.

---
+++ yimt/files/examples/tag_example.py
@@ -0,0 +1,21 @@
+from yimt.api.translators import Translators
+from yimt.files.translate_tag import translate_html
+
+html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+translator = Translators().get_translator("en", "zh")
+
+translated_soup = translate_html(translator, html_doc)
+
+print(translated_soup)
