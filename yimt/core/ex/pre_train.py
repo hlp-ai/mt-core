f745fe7d3baaf9f5cc2da0402e78431da79d7115
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-09-28 11:03:12 +0800

    Unify the output file names; Execute shell using module; Add parameters for SP training.

--- yimt/core/ex/pre_train.py
+++ yimt/core/ex/pre_train.py
@@ -4,6 +4,26 @@ import os
 
 from yimt.core.ex.sp import train_spm, load_spm, tokenize_file
 
+
+def get_file_name(p):
+    return os.path.basename(p)
+
+
+def get_sp_prefix(corpus_path, vocab_size):
+    corpus_path = get_file_name(corpus_path)
+    return "{}-sp-{}".format(corpus_path, vocab_size)
+
+
+def get_tok_file(corpus_path):
+    corpus_path = get_file_name(corpus_path)
+    return corpus_path + ".tok"
+
+
+def get_vocab_file(tok_corpus_path):
+    corpus_path = get_file_name(tok_corpus_path)
+    return corpus_path + ".vocab"
+
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("src_fn")
@@ -12,19 +32,24 @@ if __name__ == "__main__":
     argparser.add_argument("tgt_vocab_size", type=int)
     argparser.add_argument("dev_src_fn")
     argparser.add_argument("dev_tgt_fn")
+    argparser.add_argument("output_path")
+    argparser.add_argument("max_sent", type=int, default=5000000)
+    argparser.add_argument("coverage", type=float, default=0.9999)
     args = argparser.parse_args()
 
-    src_sp_prefix = args.src_fn + ".sp"
-    tgt_sp_prefix = args.tgt_fn + ".sp"
+    output_path = args.output_path
 
-    src_train_tok = args.src_fn + ".tok"
-    src_dev_tok = args.dev_src_fn + ".tok"
+    src_sp_prefix = os.path.join(output_path, get_sp_prefix(args.src_fn, args.src_vocab_size))
+    tgt_sp_prefix =os.path.join(output_path, get_sp_prefix(args.tgt_fn, args.tgt_vocab_size))
 
-    tgt_train_tok = args.tgt_fn + ".tok"
-    tgt_dev_tok = args.dev_tgt_fn + ".tok"
+    src_train_tok = os.path.join(output_path, get_tok_file(args.src_fn))
+    src_dev_tok = os.path.join(output_path, get_tok_file(args.dev_src_fn))
+
+    tgt_train_tok = os.path.join(output_path, get_tok_file(args.tgt_fn))
+    tgt_dev_tok = os.path.join(output_path, get_tok_file(args.dev_tgt_fn))
 
     print("Training SentencePiece on", args.src_fn, "and writing SP model into", src_sp_prefix)
-    train_spm(args.src_fn, src_sp_prefix, args.src_vocab_size)
+    train_spm(args.src_fn, src_sp_prefix, args.src_vocab_size, num_sentences=args.max_sent, coverage=args.coverage)
 
     print()
 
@@ -36,7 +61,7 @@ if __name__ == "__main__":
     print()
 
     print("Training SentencePiece on", args.tgt_fn, "and writing SP model into", tgt_sp_prefix)
-    train_spm(args.tgt_fn, tgt_sp_prefix, args.tgt_vocab_size)
+    train_spm(args.tgt_fn, tgt_sp_prefix, args.tgt_vocab_size, num_sentences=args.max_sent, coverage=args.coverage)
 
     print()
 
@@ -47,23 +72,23 @@ if __name__ == "__main__":
 
     print()
 
-    build_vocab_cmd = "python ../bin/build_vocab.py --size {} --save_vocab {} {}"
+    build_vocab_cmd = "python -m yimt.core.bin.build_vocab --size {} --save_vocab {} {}"
 
-    src_vocab = args.src_fn + ".voc"
+    src_vocab = os.path.join(output_path, get_vocab_file(src_train_tok))
     print("Building vocab {} from {}...".format(src_vocab, src_train_tok))
     os.popen(build_vocab_cmd.format(args.src_vocab_size, src_vocab, src_train_tok)).readlines()
 
     print()
 
-    tgt_vocab = args.tgt_fn + ".voc"
+    tgt_vocab = os.path.join(output_path, get_vocab_file(tgt_train_tok))
     print("Building vocab {} from {}...".format(tgt_vocab, tgt_train_tok))
     os.popen(build_vocab_cmd.format(args.tgt_vocab_size, tgt_vocab, tgt_train_tok)).readlines()
 
     print()
 
     print("Preparing config file...")
-    config_fn = os.path.join(os.path.dirname(args.src_fn), "config.yml")
-    model_dir = os.path.join(os.path.dirname(args.src_fn), "run")
+    config_fn = os.path.join(output_path, "config.yml")
+    model_dir = os.path.join(output_path, "model")
     config_str = """
 model_dir: {}
 
@@ -76,11 +101,13 @@ data:
 
   source_vocabulary: {}
   target_vocabulary: {}
+  
 train:
   batch_size: 4096
   max_step: 2000
   save_summary_steps: 100
   save_checkpoints_steps: 200
+  
 eval:
   steps: 200
   scorers: bleu
