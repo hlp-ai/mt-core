285f744dbcad94bca1f9ead3e0675f7b9111a97c
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-09-30 09:25:29 +0800

    Add exe for filtering.

---
+++ yimt/corpus/bin/filter.py
@@ -0,0 +1,61 @@
+import argparse
+import io
+
+from yimt.api.utils import get_logger
+from yimt.corpus.filters import JaZhFilter, LatinZhFilter
+
+
+def main(in_path, out_path, lang="en-zh"):
+    in_f = io.open(in_path, encoding="utf-8")
+    out_f = io.open(out_path, "w", encoding="utf-8")
+
+    logger = get_logger("filter")
+
+    filters = []
+    if lang == "ja-zh":
+        filters.append(JaZhFilter())
+    else:
+        filters.append(LatinZhFilter())
+
+    print(filters)
+
+    total = 0
+    passed = 0
+
+    for line in in_f:
+        total += 1
+        if total % 10000 == 0:
+            print("# pairs:", total, "# valid pairs:", passed)
+        line = line.strip()
+        cols = line.split("\t")
+        if len(cols) >= 2:
+            src, tgt = cols[:2]
+        else:
+            logger.debug("NO Pair: {}".format(line))
+            continue
+
+        valid = True
+        for f in filters:
+            if f.filter(src, tgt) is None:
+                logger.debug("{}: {}".format(f.__class__, line))
+                valid = False
+                break
+        if valid:
+            passed += 1
+            out_f.write(line + "\n")
+
+    print("# pairs:", total, "# valid pairs:", passed)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("in_fn")
+    argparser.add_argument("out_fn")
+    argparser.add_argument("lang")
+    args = argparser.parse_args()
+
+    fn = args.in_fn
+    out_fn = args.out_fn
+    lang = args.lang
+
+    main(fn, out_fn, lang)
