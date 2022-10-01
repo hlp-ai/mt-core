4b732d7d4466250911da8911348910803c788363
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-10-01 09:34:58 +0800

    Add arg parser and comment for extract_zips.py.

--- yimt/corpus/bin/extract_zips.py
+++ yimt/corpus/bin/extract_zips.py
@@ -1,5 +1,13 @@
+"""Extract OPUS zip files"""
+import argparse
+
 from yimt.corpus.utils import extract_zips
 
 
 if __name__ == "__main__":
-    extract_zips(r"D:\dataset\text\mt\zh-th")
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--dir", type=str, required=True, help="zip file dir")
+    argparser.add_argument("--output", type=str, default=None, help="output dir")
+    args = argparser.parse_args()
+
+    extract_zips(args.dir, args.output)
