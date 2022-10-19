9db0ed3f4fa8a9813db320328e27e6bf6aebf918
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-10-19 15:48:56 +0800

    Delete unused method tf_any.

--- yimt/core/utils/compat.py
+++ yimt/core/utils/compat.py
@@ -8,15 +8,6 @@ def tf_supports(symbol):
     return _string_to_tf_symbol(symbol) is not None
 
 
-def tf_any(*symbols):
-    """Returns the first supported symbol."""
-    for symbol in symbols:
-        module = _string_to_tf_symbol(symbol)
-        if module is not None:
-            return module
-    return None
-
-
 def _string_to_tf_symbol(symbol):
     modules = symbol.split(".")
     namespace = tf
