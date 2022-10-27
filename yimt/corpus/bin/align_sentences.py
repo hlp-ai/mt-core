fc713dce46c94ad8a4271c591ef71098317c8388
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-10-27 11:02:52 +0800

    Improve align_sentences.py.

--- yimt/corpus/bin/align_sentences.py
+++ yimt/corpus/bin/align_sentences.py
@@ -1,5 +1,7 @@
 import sys
 
+from yimt.api.text_splitter import split_sentences
+from yimt.api.utils import detect_lang
 from yimt.corpus.aligner import SentenceEmbeddingAligner
 
 
@@ -12,7 +14,20 @@ def get_paragraph_pair(in_fn):
     src_p = paragraphs[0::2]
     tgt_p = paragraphs[1::2]
 
-    return src_p, tgt_p
+    print("# of paragraphs:", len(src_p))
+
+    src_lang = detect_lang("".join(src_p))
+    tgt_lang = detect_lang("".join(tgt_p))
+
+    print(src_lang, "->", tgt_lang)
+
+    src_p_list = [split_sentences(p, src_lang) for p in src_p]
+    tgt_p_list = [split_sentences(p, tgt_lang) for p in tgt_p]
+
+    print("# of source sentences:", sum([len(p) for p in src_p_list]))
+    print("# of target sentences:", sum([len(p) for p in tgt_p_list]))
+
+    return src_p_list, tgt_p_list
 
 
 if __name__ == "__main__":
@@ -21,9 +36,11 @@ if __name__ == "__main__":
 
     src_p, tgt_p = get_paragraph_pair(in_fn)
 
-    out_f = open(out_fn, "a", encoding="utf-8")
+    out_f = open(out_fn, "a+", encoding="utf-8")
 
     aligner = SentenceEmbeddingAligner();
-    align = aligner.align(src_p, tgt_p)
-    for a in align:
-        out_f.write(a[0] + "\t" + a[1] + "\n")
+    for sp, tp in zip(src_p, tgt_p):
+        align = aligner.align(sp, tp)
+        for a in align:
+            out_f.write(a[0] + "\t" + a[1] + "\n")
+
