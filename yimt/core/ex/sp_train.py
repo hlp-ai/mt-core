22c9baa36fc591687e132029b2566ca9016aa481
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-11-08 10:43:07 +0800

    Improve cmd parameters for sp_train.py.

--- yimt/core/ex/sp_train.py
+++ yimt/core/ex/sp_train.py
@@ -1,13 +1,20 @@
 """Train SentencePiece model from corpus"""
 import argparse
 
+from yimt.core.ex.pre_train import get_sp_prefix
 from yimt.core.ex.sp import train_spm
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("corpus_fn")
-    argparser.add_argument("sp_model")
-    argparser.add_argument("vocab_size", type=int)
+    argparser.add_argument("--corpus", required=True, help="Corpus file path")
+    argparser.add_argument("--sp_prefix", default=None, help="SentencePiece model path prefix")
+    argparser.add_argument("--vocab_size", type=int, default=32000, help="Vocab size")
+    argparser.add_argument("--max_sentences", type=int, default=5000000, help="Max number of sentences for training")
+    argparser.add_argument("--coverage", type=float, default=0.9999, help="Vocab coverage")
     args = argparser.parse_args()
 
-    train_spm(args.corpus_fn, args.sp_model, args.vocab_size)
+    if args.sp_prefix is None:
+        sp_prefix = get_sp_prefix(args.corpus, args.vocab_size)
+
+    train_spm(args.corpus, sp_prefix, args.vocab_size,
+              coverage=args.coverage, num_sentences=args.max_sentences)
