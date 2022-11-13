8d083002b8399c19cb20fb3abedb46b79f7cb3ea
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-11-13 20:05:37 +0800

    Fix error for scoring.

--- yimt/corpus/bin/score_bitext.py
+++ yimt/corpus/bin/score_bitext.py
@@ -6,37 +6,34 @@ from yimt.corpus.bitext_scorers import LaBSEScorer
 def main(in_path, out_path):
     scorer = LaBSEScorer("D:/kidden/mt/open/mt-ex/mt/data/labse1", 48)
 
-    srcs = []
-    tgts = []
-    n_buf = 8  # consume GPU very much
-    cnt = 0
-    with open(in_path, encoding="utf-8") as in_f, open(out_path, "w", encoding="utf-8") as out_f:
-        lines = in_f.readlines()
-        print("# of lines:", len(lines))
-        for line in lines:
+    lines = open(in_path, encoding="utf-8").readlines()
+    print("# of lines:", len(lines))
+
+    out_f = open(out_path, "w", encoding="utf-8")
+
+    block = 4
+    n = 0
+    for i in range(0, len(lines), block):
+        buf = lines[i:i+block]
+        srcs = []
+        tgts = []
+        for line in buf:
             line = line.strip()
             pair = line.split("\t")
             src = pair[0]
             tgt = pair[1]
+            srcs.append(src)
+            tgts.append(tgt)
 
-            if len(srcs) < n_buf:
-                srcs.append(src)
-                tgts.append(tgt)
-            else:
-                ss = scorer.score(srcs, tgts)
-                for i in range(len(ss)):
-                    out_f.write("{:.4f}\t{}\t{}\n".format(ss[i], srcs[i], tgts[i]))
-                srcs.clear()
-                tgts.clear()
-                print(cnt)
+        ss = scorer.score(srcs, tgts)
+        for j in range(len(ss)):
+            out_f.write("{:.4f}\t{}\t{}\n".format(ss[j], srcs[j], tgts[j]))
 
-            cnt += 1
+        n += len(buf)
+        print(n)
 
-        if len(srcs) > 0:
-            ss = scorer.score(srcs, tgts)
-            for i in range(len(ss)):
-                out_f.write("{:.4f}\t{}\t{}\n".format(ss[i], srcs[i], tgts[i]))
-        print(cnt)
+    print(n)
+    out_f.close()
 
 
 if __name__ == "__main__":
