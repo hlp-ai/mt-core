bbeb088d45bdf614a23680c711d895aaaff636d6
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-12-14 10:24:29 +0800

    Add more test examples for text split.

--- yimt/api/examples/split_api.py
+++ yimt/api/examples/split_api.py
@@ -27,6 +27,10 @@ installation methods and basic operations.
 
     print(may_combine_paragraph(t4))
 
+    print(word_segment("I'm a teacher. This is not co-exist.", lang="en"))
+
     t5 = "이에 따라 부산세계탁구선수권대회는 2024년 5월 24일부터 6월 2일까지 열흘 간 부산 해운대구 벡스코에서 열린다. 세계탁구선수권대회는 단일 종목으로는 가장 많은 100여개 국 2000여 명의 선수, 임원이 참여해 월드컵에 버금가는 글로벌 이벤트다. 홀수 해는 남녀 단·복식, 혼합복식 5종목이 열리고, 짝수 해에는 남녀 단체전이 진행된다."
     print(word_segment(t5, lang="ko"))
     print(split_sentences(t5, lang="ko"))
+
+    print(word_segment("Viện trưởng Vương xử sự ngay thẳng, làm việc công minh, chưa từng gây khó dễ cho ai, mọi người đều tôn kính ông ấy.", lang="vi"))
