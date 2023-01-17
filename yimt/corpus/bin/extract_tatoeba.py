61b157048e4db5b5ec362559ffffdab2013f096a
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-01-17 10:24:13 +0800

    Extract lines from Tatoeba with specified source and target languages.

--- yimt/corpus/bin/extract_tatoeba.py
+++ yimt/corpus/bin/extract_tatoeba.py
@@ -1,10 +1,18 @@
+import argparse
 import sys
 
 if __name__ == "__main__":
-    in_fn = sys.argv[1]
-    out_fn = sys.argv[2]
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-i", "--input", required=True, help="Input file path")
+    argparser.add_argument("-o", "--output", required=True, help="Output file path")
+    argparser.add_argument("-sl", "--source_langs", nargs="*", help="Choose lines with these source languages")
+    argparser.add_argument("-tl", "--target_langs", nargs="*", help="Choose lines with these target languages")
+    args = argparser.parse_args()
+    in_fn = args.input
+    out_fn = args.output
 
-    tgt_lang = ["cmn_Hans", "cmn_Hant"]
+    tgt_lang = args.target_langs  # ["cmn_Hans", "cmn_Hant"]
+    src_lang = args.source_langs
 
     outf = open(out_fn, "w", encoding="utf-8")
 
@@ -12,7 +20,13 @@ if __name__ == "__main__":
         for line in inf:
             line = line.strip()
             segs = line.split("\t")
-            if segs[1] in tgt_lang:
-                outf.write(segs[2] + "\t" + segs[3] + "\n")
+
+            if tgt_lang is not None and segs[1] not in tgt_lang:
+                continue
+
+            if src_lang is not None and segs[0] not in src_lang:
+                continue
+
+            outf.write(segs[2] + "\t" + segs[3] + "\n")
 
     outf.close()
