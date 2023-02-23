c836015a546a1263897030eff14bd2b063fcbed6
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-02-23 19:35:39 +0800

    Output filtered pairs into file instead of logging.

--- yimt/corpus/bin/filter.py
+++ yimt/corpus/bin/filter.py
@@ -42,30 +42,33 @@ def main(in_path, out_path, lang_pair):
     total = 0
     passed = 0
 
-    for line in in_f:
-        total += 1
-        if total % 10000 == 0:
-            print("# pairs:", total, "# valid pairs:", passed)
-        line = line.strip()
-        cols = line.split("\t")
-        if len(cols) >= 2:
-            src, tgt = cols[:2]
-        else:
-            logger.debug("NO Pair: {}".format(line))
-            continue
+    with open(in_path, encoding="utf-8") as in_f, open(out_path, "w", encoding="utf-8") as out_f, \
+            open(in_path+".filtered", "w", encoding="utf-8") as filtered_f:
+        for line in in_f:
+            total += 1
+            if total % 10000 == 0:
+                print("# pairs:", total, "# valid pairs:", passed)
+            line = line.strip()
+            cols = line.split("\t")
+            if len(cols) >= 2:
+                src, tgt = cols[:2]
+            else:
+                # logger.debug("NO Pair: {}".format(line))
+                filtered_f.write(line + "\n")
+                continue
 
-        valid = True
-        for f in filters:
-            if f.filter(src, tgt) is None:
-                logger.debug("{}: {}".format(f.__class__, line))
-                valid = False
-                break
-        if valid:
-            passed += 1
-            out_f.write(line + "\n")
+            valid = True
+            for f in filters:
+                if f.filter(src, tgt) is None:
+                    # logger.debug("{}: {}".format(f.__class__, line))
+                    filtered_f.write(line + "\n")
+                    valid = False
+                    break
+            if valid:
+                passed += 1
+                out_f.write(line + "\n")
 
     print("# pairs:", total, "# valid pairs:", passed)
-    out_f.close()
 
 
 if __name__ == "__main__":
