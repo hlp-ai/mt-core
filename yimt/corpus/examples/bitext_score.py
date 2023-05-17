6717b4cb3487a0d51cfb132b64dcedb2364555fa
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-05-17 17:15:16 +0800

    Delete example code for non-existing LaserScorer.

--- yimt/corpus/examples/bitext_score.py
+++ yimt/corpus/examples/bitext_score.py
@@ -1,6 +1,6 @@
 import time
 
-from yimt.corpus.bitext_scorers import LaBSEScorer, LaserScorer
+from yimt.corpus.bitext_scorers import LaBSEScorer
 
 if __name__ == "__main__":
     scorer = LaBSEScorer("D:/kidden/mt/open/mt-ex/mt/data/labse1", 72)
@@ -12,11 +12,3 @@ if __name__ == "__main__":
     print(scorer.score(english_sentences, chinese_sentences))
     print(time.time() - start)
 
-    scorer = LaserScorer()
-
-    english_sentences = ["dog", "Puppies are nice.", "I enjoy taking long walks along the beach with my dog."]
-    chinese_sentences = ["狗", "小狗很好。", "我喜欢带着我的狗沿着海滩散步。"]
-
-    start = time.time()
-    print(scorer.score(english_sentences, chinese_sentences))
-    print(time.time() - start)
