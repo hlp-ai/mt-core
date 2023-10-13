26815c237fd0e61bf8d7faaa66f6c7129739354a
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-10-13 09:29:42 +0800

    Move codes into mt-data.

--- yimt/corpus/examples/noletter.py
+++
@@ -1,4 +0,0 @@
-from yimt.corpus.dedup import norm
-
-
-print(norm("平均両替レート：38.1907	平均汇率：38.1907"))
--- yimt/corpus/examples/normalizer_test.py
+++
@@ -1,6 +0,0 @@
-from yimt.corpus.normalizers import Cleaner
-
-
-cleaner = Cleaner()
-print(cleaner.normalize("a  bc\tthis is a test."))
-print(cleaner.normalize("\u0001 a bc\t this ia中文"))
