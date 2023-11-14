d9c9d9775f233bc5ccafac4b7c20184d48b09900
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-11-14 10:14:57 +0800

    Add resample.py.

---
+++ yimt/experimental/mnmt/resample.py
@@ -0,0 +1,53 @@
+"""对各种语言单语语料进行重采样以产生SentencePiece训练语料"""
+import argparse
+import os
+
+from yimt.utils.misc import count_lines, sample, upsample
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root", required=True, help="monolingual files directory")
+    parser.add_argument("--t", type=float, default=5.0, help="sampling temperature")
+    args = parser.parse_args()
+
+    root = args.root
+
+    files = os.listdir(root)
+    files = [os.path.join(root, f) for f in files]
+
+    counts_raw = []
+    for f in files:
+        print("Counting lines:", f)
+        n = count_lines(f)
+        print("  ", f, ":", n)
+        counts_raw.append(n)
+
+    total = sum(counts_raw)
+    print("Total lines:", total)
+
+    probs_raw = []
+    print("Raw counts and probabilities")
+    for i, f in enumerate(files):
+        p = counts_raw[i] / float(total)
+        probs_raw.append(p)
+        print("  ", f, counts_raw[i], p)
+
+    T = 1 / args.t
+    probs_raw = [p ** T for p in probs_raw]
+    prob_total = sum(probs_raw)
+    probs_normalized = [p / prob_total for p in probs_raw]
+
+    counts_normalized = [int(p * total) for p in probs_normalized]
+    print("Normalized counts and probabilities")
+    for i, f in enumerate(files):
+        print("  ", f, counts_normalized[i], probs_normalized[i])
+
+    for i in range(len(counts_raw)):
+        if counts_normalized[i] < counts_raw[i]:
+            print("Sampling", files[i], counts_normalized[i])
+            sample([files[i]], counts_normalized[i])
+        else:
+            print("Upsampling", files[i], counts_normalized[i])
+            upsample([files[i]], counts_normalized[i])
+
