f7dfd861d03f3f14284ecba762fd6c4b42ef25e7
liuxf_hust <hlp_ai@sina.com>
2023-12-07 10:36:19 +0800

    Add add_tag_batch.py.

---
+++ yimt/experimental/mnmt/add_tag_batch.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+
+from yimt.experimental.mnmt.add_tag import add_token
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tsv_dir", required=True, help="tsv file dir")
+    parser.add_argument("--to", default="tgt", type=str, help="add token to src or tag of tsv file")
+    args = parser.parse_args()
+
+    input = args.tsv_file
+    tosrc = True
+    if args.to == "tgt":
+        tosrc = False
+
+    tsv_files = os.listdir(input)
+    for f in tsv_files:
+        i = f.index("-")
+        lang = f[:i]
+        token = "<to" + lang + ">"
+        f = os.path.join(input, f)
+        print(f, tosrc, token)
+        add_token(f, tosrc, token)
