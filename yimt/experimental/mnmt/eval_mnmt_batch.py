d41558dc0baaca3e8e18a3db7e73bd76bc0dab7b
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-12-25 09:13:54 +0800

    Fix eval_mnmt_batch.py.

--- yimt/experimental/mnmt/eval_mnmt_batch.py
+++ yimt/experimental/mnmt/eval_mnmt_batch.py
@@ -7,7 +7,7 @@ from yimt.utils.misc import pair_to_single
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--tsv_dir", default=r"D:\dataset\mnmt\dev\tsv2", help="eval files dir")
-    argparser.add_argument("--x2zh", default=True, help="X-ZH OR ZH-X")
+    argparser.add_argument("--direction", default="x2zh", help="x2zh or zh2x")
     argparser.add_argument("--sp_x", default= r"D:\dataset\mnmt\sp\cc50.x.sampled-sp-48000.model",
                            help="SentencePiece model path for x language")
     argparser.add_argument("--sp_zh", default=r"D:\dataset\mnmt\sp\en-zh.tsv.zh-sp-32000.model",
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     argparser.add_argument("--config", required=True, help="Config file path")
     args = argparser.parse_args()
 
-    if args.x2zh:
+    if args.direction == "x2zh":
         x2zh = True
         sp_src = args.sp_x
         sp_tgt = args.sp_zh
@@ -45,8 +45,8 @@ if __name__ == "__main__":
             src_path = zh_fn
             ref_path = x_fn
 
-        print(f, zh_fn, x_fn)
+        print(lang_pair, f, zh_fn, x_fn)
 
-        pair_to_single(lang_pair, f, zh_fn, x_fn)
+        pair_to_single(f, zh_fn, x_fn)
 
         run_eval(conf, src_path, ref_path, sp_src, sp_tgt, lang_pair)
