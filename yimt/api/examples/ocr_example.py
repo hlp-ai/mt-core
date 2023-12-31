35ac6902fec162f8dd9c8759f9ba7a88c89c2708
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-12-31 16:35:52 +0800

    Update ocr_example.py.

--- yimt/api/examples/ocr_example.py
+++ yimt/api/examples/ocr_example.py
@@ -1,9 +1,9 @@
 from ocr.detect import OCRImpl
 
 app = OCRImpl(ctpn_weight_path=r"D:\kidden\mt\open\github\mt-io\ocr\weights\weights-ctpnlstm-init.hdf5",
-                  densenet_weight_path=r"D:\kidden\mt\ocr\latin\latin_dense.hdf5",
-                  dict_path=r"D:\kidden\mt\open\github\mt-io\ocr\dictionary\latin_chars.txt")
+                  densenet_weight_path=r"D:\kidden\mt\ocr\latin\densenet.hdf5",
+                  dict_path=r"D:\kidden\mt\ocr\latin\data\vocab-Latn.txt")
 
-image_path = "./en1.png"
+image_path = "./es1.png"
 texts = app.detect(image_path)
 print('\n'.join(texts))
