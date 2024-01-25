a175f23a3346324e12f947935a50082e4076a2e7
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-01-25 10:31:32 +0800

    补充通用js代码文件；提取设置框js代码。

--- yimt/service/static/common_block.js
+++ yimt/service/static/common_block.js
@@ -52,5 +52,33 @@ function set_setting(){
         </div>
         <input type="button" class="url_setting_hide" value="关闭" onclick="url_setting_hide_func()">
         <input type="button" class="url_button" id="url_button" value="设置" onclick="url_resetting_func()">
-    `
+    `;
+
+    document.getElementById("url_setting_label").addEventListener("click", url_setting_func);
+}
+
+function url_setting_func()
+{
+    document.getElementById("url_setting_block").style.visibility='visible';
+    document.getElementById("mask").style.display='block';
+    document.getElementById("url").value=END_POINT;
+    document.getElementById("api_key").value=API_KEY;
+}
+
+function url_resetting_func()
+{
+    END_POINT = document.getElementById("url").value;
+    window.localStorage.setItem("server", END_POINT);
+
+    document.getElementById("url_setting_block").style.visibility='hidden';
+    document.getElementById("mask").style.display='none';
+
+    API_KEY = document.getElementById("api_key").value;
+    window.localStorage.setItem("key",API_KEY);
+}
+
+function url_setting_hide_func()
+{
+    document.getElementById("url_setting_block").style.visibility='hidden';
+    document.getElementById("mask").style.display='none'
 }
